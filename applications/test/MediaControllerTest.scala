bb1914074a426b6f5f48d91614e27de860f3025a
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2016-05-20 10:37:53 +0100

    Content-type handling changes

--- applications/test/MediaControllerTest.scala
+++ applications/test/MediaControllerTest.scala
@@ -27,7 +27,7 @@ import org.scalatest.{BeforeAndAfterAll, DoNotDiscover, FlatSpec, Matchers}
 
     val result = mediaController.render(videoUrl)(fakeRequest)
     status(result) should be(200)
-    header("Content-Type", result).get should be("application/json; charset=utf-8")
+    contentType(result) shouldBe Some("application/json")
     contentAsString(result) should startWith("{\"config\"")
   }
 
